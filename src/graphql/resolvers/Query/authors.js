a782e8ef367069eec2f494182eb4a1dcd28ebc73
Nick Little <149431438+nlittle-pella@users.noreply.github.com>
2024-05-17 12:17:31 -0500

    feat: naive approach -- always fetch the books

---
+++ src/graphql/resolvers/Query/authors.js
@@ -0,0 +1,54 @@
+const authors = [
+    {
+        id: 1,
+        name: 'John Doe',
+    },
+    {
+        id: 2,
+        name: 'Jane Doe',
+    },
+    {
+        id: 3,
+        name: 'Alice Doe',
+    },
+];
+const booksByAuthor = {
+    [authors[0].id]: [
+        {
+            id: 1,
+            title: `${authors[0].name} Book 1`,
+        },
+        {
+            id: 2,
+            title: `${authors[0].name} Book 2`,
+        },
+    ],
+    [authors[1].id]: [
+        {
+            id: 3,
+            title: `${authors[1].name} Book 1`,
+        },
+        {
+            id: 4,
+            title: `${authors[1].name} Book 2`,
+        },
+    ],
+    [authors[2].id]: [
+        {
+            id: 5,
+            title: `${authors[2].name} Book 1`,
+        },
+    ],
+};
+
+const getBooks = (authorId) => {
+    // eslint-disable-next-line no-console
+    console.log('Fetching books for author:', authorId);
+
+    return booksByAuthor[authorId] || [];
+};
+
+export const handler = () => authors.map((author) => ({
+    ...author,
+    books: getBooks(author.id),
+}));
