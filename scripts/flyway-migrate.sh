1120d840bef801b826edebae83d5aae0d6f88e74
Nick Little <149431438+nlittle-pella@users.noreply.github.com>
2024-07-30 08:09:58 -0500

    fix: get migrations working

--- scripts/flyway-migrate.sh
+++ scripts/flyway-migrate.sh
@@ -11,8 +11,9 @@ then
     -url='jdbc:postgresql://localhost:5432/authorapidb' -locations=filesystem:/flyway/sql
 else
   docker run \
+    --platform linux/amd64 \
     -v ${flyway_script_path}/migrations:/flyway/sql:ro \
-    --network author-api_default \
+    --network fun-with-dataloaders_default \
     --rm flyway/flyway:9.1.4-alpine \
     migrate -user=flyway -password=flyway_ps -connectRetries=10 \
     -url='jdbc:postgresql://author-db:5432/author-api' -locations=filesystem:/flyway/sql
